3a9a15a3b3fb2d6d7c0cde64670c002c94076e84
stanls <stanley_sie@dlsu.edu.ph>
2019-08-21 21:45:12 +0800

    fixing deployment - 22

--- model/database.js
+++ model/database.js
@@ -12,7 +12,7 @@ class Database {
     }
     async query( sql, args ) {
         try {
-            const result = await new Promise((resolve, reject) => {
+            return await new Promise((resolve, reject) => {
                 this.connection.query(sql, args, (err, rows) => {
                     if (err)
                         return reject(err);
