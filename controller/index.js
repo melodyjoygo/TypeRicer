2b401ac243b5e426583bef070dc7ae57e4045397
stanls <stanley_sie@dlsu.edu.ph>
2019-08-21 21:36:07 +0800

    fixing deployment - 20

--- controller/index.js
+++ controller/index.js
@@ -43,8 +43,7 @@ router.post("/login", (req, res) => {
 })
 router.get("/guest", (req, res) => {
     Promise.resolve(User.getUser('guest')).then(function(value) {
-        console.log(value);
-        req.session.username = value[0].username;
+        req.session.username = value.username;
         res.clearCookie("username");
         res.clearCookie("password");
         res.redirect("/typericer");
