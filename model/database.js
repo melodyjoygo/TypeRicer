2ad1a6fc01d376f0eae26f8b1070210b36df9b2c
stanls <stanley_sie@dlsu.edu.ph>
2019-08-21 21:16:59 +0800

    fixing deployment - 12

--- model/database.js
+++ model/database.js
@@ -10,8 +10,8 @@ class Database {
             database: 'typericer'
         });
     }
-    query( sql, args ) {
-        return new Promise((resolve, reject) => {
+    async query( sql, args ) {
+        return await new Promise((resolve, reject) => {
             this.connection.query(sql, args, (err, rows) => {
                 if (err)
                     return reject(err);
