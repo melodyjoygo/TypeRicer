ba760411a8a84b288bfd888134aa4ef400e3df95
stanls <stanley_sie@dlsu.edu.ph>
2019-08-21 15:28:38 +0800

    fixing deployment

--- model/database.js
+++ model/database.js
@@ -12,12 +12,16 @@ class Database {
     }
     query( sql, args ) {
         return new Promise( ( resolve, reject ) => {
-            this.connection.query( sql, args, ( err, rows ) => {
-                if ( err )
-                    return reject( err );
+            try {
+                this.connection.query( sql, args, ( err, rows ) => {
+                    if ( err )
+                        return reject( err );
+                    
+                    resolve( rows );
+                });
+            } catch(err) {
                 
-                resolve( rows );
-            });
+            }
         });
     }
     close() {
